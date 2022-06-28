5e41f15af629d25c0c188dedf46daea24647af63
Martin Novice <96988126+Novicce@users.noreply.github.com>
2022-06-28 10:33:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,76 +1,108 @@
+"""
+
+projekt_1.py: druhý projekt do Engeto Online Python Akademie
+
+
+autor: Martin Nováček
+
+email: mnovacek@me.com
+
+discord: Novicce #7276
+
+"""
+
+
 import random
+import time
+from tokenize import Number
 
-# of a number
-def gen_cisel(cislo):
-    return [int(i) for i in str(cislo)]
-      
-  
-# Returns True if number has 
-# no duplicate digits 
-# otherwise False      
-def neDuplicitni(cislo):
-    cislo_je = gen_cisel(cislo)
-    if len(cislo_je) == len(set(cislo_je)):
-        return True
+
+ODDELOVAC = ("-" * 49)
+
+#generator cisel =====================================
+
+def main():
+    total_time = game_counter = all_counter = 0
+    x = None
+    while x != 'q':  
+
+        
+        while True:  
+            number = 2017
+            random.randrange(1234, 9877)
+            if len(set(str(number))) == 4:
+                print(number)
+            break
+
+        start = time.time()
+        counter = 0
+        num = None
+        while str(num) != str(number):
+            num = get_num()
+            match(num, number)
+            counter += 1
+        end = time.time()
+        
+        print(f"Correct, you've guessed the right number in {counter} guesses!")
+        print(f"This game took {round((end - start), 2)} seconds")
+        
+        # Hrat znovu
+        again = ""
+        while not (again == "Y" or again == "N"):
+            again = input("Do you want to play again? (Y/N) ").upper()
+        if again == "N":
+            print("Thank you for playing! Bye bye.")
+            break
+        
+        print()
+        game_counter += 1
+        total_time += (end - start)
+        all_counter += counter
+        
+        
+def match(num, number):
+    bulls = cows = 0
+    for i in range(4):
+        if str(number)[i] in set(str(num)):
+            cows += 1
+        if str(number)[i] == str(num)[i]:
+            bulls += 1
+            cows -= 1
+    if bulls == 1:
+        print(bulls, "bull")
+        print(ODDELOVAC)
     else:
-        return False
-  
-  
-# Generates a 4 digit number 
-# with no repeated digits    
-def vystup_cisel():
-    while True:
-        num = random.randint(1000,9999)
-        if neDuplicitni(num):
-            return num
-  
-  
-# Returns common digits with exact 
-# matches (bulls) and the common 
-# digits in wrong position (cows)
-def pocet_bull_cows(cislo,hadej):
-    bull_cow = [0,0]
-    cislo_je= gen_cisel(cislo)
-    hadej_je = gen_cisel(hadej)
-      
-    for i,j in zip(cislo_je,hadej_je):
-          
-        # common digit present
-        if j in cislo_je:
-          
-            # common digit exact match
-            if j == i:
-                bull_cow[0] += 1
-              
-            # common digit match but in wrong position
-            else:
-                bull_cow[1] += 1
-                  
-    return bull_cow
+        print(bulls, "bulls")
+        print(ODDELOVAC)
+    if cows == 1:
+        print(cows, "cow" )
+        print(ODDELOVAC)
+    else:
+        print(cows, "cows" )
+        print(ODDELOVAC)
 
-      
-# Secret Code
-num = vystup_cisel()
-tries =int(input('Enter number of tries: '))
-  
-# Play game until correct guess 
-# or till no tries left
-while tries > 0:
-    hadej = int(input("Enter your guess: "))
-      
-    if not neDuplicitni(hadej):
-        print("Number should not have repeated digits. Try again.")
-        continue
-    if hadej < 1000 or hadej > 9999:
-        print("Enter 4 digit number only. Try again.")
-        continue
-      
-    bull_cow = pocet_bull_cows(num,hadej)
-    print(f"{bull_cow[0]} bulls, {bull_cow[1]} cows")
-    tries -=1
-      
-    if bull_cow[0] == 4:
-        print("You guessed right!")
-        break
-else:
-    print(f"You ran out of tries. Number was {num}")
+#========================================================
+
+print(ODDELOVAC)
+print("Hi there!")
+print(ODDELOVAC)
+print(
+    "I've generated a random 4 digit number for you,",
+    "Let's play a bulls and cows game.",
+    sep="\n"
+)
+print(ODDELOVAC)
+
+#========================================================
+
+def get_num():
+    while True:
+        num = input(f'Enter a number: ')
+        if len(set(str(num))) == 4 and num.isnumeric():
+            return num
+
+
+main()
+print()
+
+
